5b65d6c20d5db1eab11f55b85bb9fc87f65331a6
unknown <deepakchacko2700@gmail.com>
2023-01-06 12:32:31 +0530

    delete travel project' :

--- travel_project/credentials/urls.py
+++
@@ -1,8 +0,0 @@
-from . import views
-from django.urls import path
-
-urlpatterns=[
-    path("registration", views.register, name="registration"),
-    path("login", views.login, name="login"),
-    path("logout", views.logout, name="logout"),
-]
